abe67654b169a98457d7296a6ce7df8f6154c953
Natacha Vergara <ntchvergara@gmail.com>
2022-07-25 13:11:15 -0300

    actualice el main css a module.scss y toque un poco de estilo

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,13 @@
-
-import './Main.css'
+import styles from './Main.module.scss'
 
 const Main = () => {
   return (
-    <main>
-      <p id="cita"> 
-      <h1 id="bienvenida" className='text-center'>Pablo Lopez - Terapia analítica</h1>
-      <br />
-      
-      <i> (...) Le debo al psicoanálisis entender la vida como un desafío el que uno no puede sentirse víctima; en definitiva, el psicoanálisis me ha enseñado que uno debe entregarse durante toda una vida a una tarea imposible: aceptar las consecuencias imprevisibles de lo que uno elige.</i></p>
+    <main className={styles.main}>
+      <p id={styles.cita}>
+        <h1 id="bienvenida" className='text-center'>Pablo Lopez - Terapia analítica</h1>
+        <br />
+
+        <i> (...) Le debo al psicoanálisis entender la vida como un desafío el que uno no puede sentirse víctima; en definitiva, el psicoanálisis me ha enseñado que uno debe entregarse durante toda una vida a una tarea imposible: aceptar las consecuencias imprevisibles de lo que uno elige.</i></p>
     </main>
   );
 }
